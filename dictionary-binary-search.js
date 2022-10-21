25cda05da3a093d4059c19b621915833fff92871
axt12152 <axt12152@castleton.edu>
2022-10-21 10:49:48 -0400

    dictionary tests

--- dictionary-binary-search.js
+++ dictionary-binary-search.js
@@ -1,13 +1,27 @@
 import words from "./words.js";
 
-function binarySearch(needle, haystack) 
-    for(let i= 10; i < haystack.length; i++) {
-      if (needle == haystack[i]) {
-        return true;
-      }
-        return false;
-}
+function binarySearch(needle, haystack) {
+    let start = 0;             //Green bookmark
+    let end = haystack.length; //red bookmark {
+      
+  while ( start < end ){
+     let middle = (start + end) / 2;
+     middle = Math.floor(middle);
+     let middleWord = haystack[middle]; 
 
+     if (middleWord == needle) {
+         return true; //Found it!!!
+
+     }
+     
+     if ( middleWord < needle ){
+         start = middle + 1;
+     } else if (middleWord > needle ){
+         end = middle; 
+     }
+  }
+
+  return false, //did not find the word
 
 export default function(word){
     return binarySearch(word, words);
