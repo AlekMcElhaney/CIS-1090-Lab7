5eaa6f287bcc6d904a752f678899fb40ee531b33
Alek McElhaney <112564666+AlekMcElhaney@users.noreply.github.com>
2022-10-30 18:55:33 -0400

    Add files via upload
    Completed the code but could not get it to run tests without problems

---
+++ personal-dictionary.js
@@ -0,0 +1,29 @@
+function search(needle, haystack) {
+    let start = 0;
+    let end = haystack.length;
+    while (start < end) {
+
+
+
+
+        function addWord(needle, haystack) {
+            let start = 0;
+            let end = haystack.length;
+
+            for let i = start; i < end; i++) {
+                if (needle == haystack[i]) {
+                    return true;
+
+                }
+                return false;
+            }
+
+            export default function (word) {
+                return linearSearch(word, words);
+            }
+
+        }
+
+    }
+
+
